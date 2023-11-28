d7ff1f1ac675b4387bce42d16e42c75128f1cbcf
edwardphillip <112529094+edwardphillip@users.noreply.github.com>
2023-11-27 17:12:32 -0800

    Map Modified

--- src/app/home/maps.tsx
+++ src/app/home/maps.tsx
@@ -1,19 +1,72 @@
-// require("dotenv").config();
-// import { Client } from "@googlemaps/google-maps-services-js";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl, { LngLatLike } from 'mapbox-gl';
 
-// const client = new Client({});
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGJlbnNvIiwiYSI6ImNsbDV2dTl0NjBjYzMzcnM4NTdrMDZyMTgifQ.VcDVcpA5edcvU_Ao7auekQ';
 
-// client
-//   .elevation({
-//     params: {
-//       locations: [{ lat: 45, lng: -110 }],
-//       key: process.env.GOOGLE_MAPS_API_KEY as string,
-//     },
-//     timeout: 1000, // milliseconds
-//   })
-//   .then((r) => {
-//     console.log(r.data.results[0].elevation);
-//   })
-//   .catch((e) => {
-//     console.log(e.response.data.error_message);
-//   });
+interface Store {
+  name: string;
+  coordinates: LngLatLike;
+}
+
+interface MapProps {
+  liveLocation: LngLatLike;
+}
+
+export default function Maps({ liveLocation }: MapProps) {
+  const [stores, setStores] = useState<Store[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/stores'); // Replace with your API endpoint
+        const data = await response.json();
+        setStores(data.result);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching store data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map-container",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: liveLocation,
+      zoom: 12,
+    });
+
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+      })
+    );
+
+    if (!loading && stores.length > 0) {
+      stores.forEach((store) => {
+        new mapboxgl.Marker()
+          .setLngLat(store.coordinates)
+          .setPopup(new mapboxgl.Popup().setHTML(`<h3>${store.name}</h3>`))
+          .addTo(map);
+      });
+    }
+
+    return () => {
+      map.remove();
+    };
+  }, [liveLocation, stores, loading]);
+
+  return (
+    <div id="map-container" style={{ height: "400px" }}>
+      {loading && <p>Loading...</p>}
+    </div>
+  );
+}
