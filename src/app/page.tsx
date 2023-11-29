daffe2a4e7329c4d8e7cd9bc16d220c0b3d8b048
edwardphillip <112529094+edwardphillip@users.noreply.github.com>
2023-11-29 14:37:18 -0800

    revert styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-"use client";
+import { Metadata } from "next";
 import Home from "./home/page";
-import React, { useEffect, useState } from "react"; // Import React
+
+export const metadata: Metadata = {
+  title: "Welcome to TalkCoffee!",
+  description: "A website for finding craft coffee.",
+};
 
 export default function App() {
-  const [isClient, setIsClient] = useState(false)
- 
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
-
-  return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="max-w-md p-6 bg-white rounded-md shadow-md">
-        <h1 className="text-3xl font-bold mb-4 text-center">Welcome to TalkCoffee!</h1>
-        <p className="text-gray-600 text-center">A website for finding craft coffee.</p>
-        <Home />
-      </div>
-    </div>
-  );
+  return <Home />;
 }
