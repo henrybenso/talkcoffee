0b34c8adc643bd84f10763bcab13c1102540a9f7
henrybenso <henrykbenso1@gmail.com>
2024-06-06 16:10:37 -0700

    page refactor

--- src/app/store/create-by-owner/page.tsx
+++ src/app/store/create-by-owner/page.tsx
@@ -12,6 +12,7 @@ import { Controller, useForm } from "react-hook-form";
 import { useState } from "react";
 import Select from "react-select";
 import { schema } from "@/app/validation";
+import FormField from "./formField";
 
 interface Days {
   sunday: "SUN";
@@ -42,101 +43,155 @@ const dineOptions = [
 ];
 
 export default function CreateStore() {
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-    getValues,
-  } = useForm({
-    resolver: zodResolver(schema),
-    defaultValues: {
-      name: "",
-      rating: "5",
-      phoneNumber: "",
-      instagramHandle: "",
-      avatar: "",
-      images: "",
-      serviceTypes: {
-        sitIn: [dineOptions[0]],
-        takeOut: { value: false, label: "No" },
-        delivery: { value: false, label: "No" },
-        curbsidePickup: { value: false, label: "No" },
-      },
-      serviceHours: {
-        sunday: {
-          open: "",
-          close: "",
-        },
-        monday: {
-          open: "",
-          close: "",
-        },
-        tuesday: {
-          open: "",
-          close: "",
-        },
-        wednesday: {
-          open: "",
-          close: "",
-        },
-        thursday: {
-          open: "",
-          close: "",
-        },
-        friday: {
-          open: "",
-          close: "",
-        },
-        saturday: {
-          open: "",
-          close: "",
-        },
-      },
-    },
-  });
+  // const {
+  //   register,
+  //   control,
+  //   handleSubmit,
+  //   formState: { errors },
+  //   getValues,
+  // } = useForm({
+  //   resolver: zodResolver(schema),
+  //   defaultValues: {
+  //     name: "",
+  //     rating: "5",
+  //     phoneNumber: "",
+  //     instagramHandle: "",
+  //     avatar: "",
+  //     images: "",
+  //     serviceTypes: {
+  //       sitIn: [dineOptions[0]],
+  //       takeOut: { value: false, label: "No" },
+  //       delivery: { value: false, label: "No" },
+  //       curbsidePickup: { value: false, label: "No" },
+  //     },
+  //     serviceHours: {
+  //       sunday: {
+  //         open: "",
+  //         close: "",
+  //       },
+  //       monday: {
+  //         open: "",
+  //         close: "",
+  //       },
+  //       tuesday: {
+  //         open: "",
+  //         close: "",
+  //       },
+  //       wednesday: {
+  //         open: "",
+  //         close: "",
+  //       },
+  //       thursday: {
+  //         open: "",
+  //         close: "",
+  //       },
+  //       friday: {
+  //         open: "",
+  //         close: "",
+  //       },
+  //       saturday: {
+  //         open: "",
+  //         close: "",
+  //       },
+  //     },
+  //   },
+  // });
+
+  const [error, setError] = useState("");
+  const [status, setStatus] = useState("typing");
+
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState(5);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [instagramHandle, setInstagramHandle] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [images, setImages] = useState([]);
+  const [sitIn, setSitIn] = useState(false);
+  const [takeOut, setTakeOut] = useState(false);
+  const [delivery, setDelivery] = useState(false);
+  const [curbsidePickup, setCurbsidePickup] = useState(false);
 
   const [fileLimit, setFileLimit] = useState(false);
   const selectUniqueId = Date.now().toString();
 
-  const handleGetValues = () => {
-    console.log("Get Values", getValues());
-  };
+  // const _onSubmit = (data: any) => {
+  //   // alert(JSON.stringify(data));
+  //   console.log(data);
+  // };
 
-  const _onSubmit = (data: any) => {
-    // alert(JSON.stringify(data));
-    console.log(data);
-  };
+  async function onSubmit(e: React.FormEvent) {
+    const {
+      name,
+      rating,
+      phoneNumber,
+      instagramHandle,
+      avatar,
+      images,
+      serviceTypes,
+      serviceHours,
+    } = values;
 
-  // async function submitForm(values: z.infer<typeof schema>) {
-  //   const {
-  //     name,
-  //     rating,
-  //     phoneNumber,
-  //     instagramHandle,
-  //     avatar,
-  //     images,
-  //     serviceTypes,
-  //     serviceHours,
-  //   } = values;
+    const formData = new FormData();
+    console.log(avatar[0]);
+    formData.append("avatar", avatar[0]);
+    formData.append("imageOne", images[0]);
+    formData.append("imageTwo", images[1]);
+    formData.append("imageThree", images[2]);
+    formData.append("imageFour", images[3]);
+    formData.append("imageFive", images[4]);
+    formData.append("store", JSON.stringify(values));
 
-  //   const formData = new FormData();
-  //   console.log(avatar[0]);
-  //   formData.append("avatar", avatar[0]);
-  //   formData.append("imageOne", images[0]);
-  //   formData.append("imageTwo", images[1]);
-  //   formData.append("imageThree", images[2]);
-  //   formData.append("imageFour", images[3]);
-  //   formData.append("imageFive", images[4]);
-  //   formData.append("store", JSON.stringify(values));
+    const res = await fetch("http://localhost:3000/api/store", {
+      method: "POST",
+      body: formData,
+    });
+    const result = await res.json();
+    console.log(result);
+  }
 
-  //   const res = await fetch("http://localhost:3000/api/store", {
-  //     method: "POST",
-  //     body: formData,
-  //   });
-  //   const result = await res.json();
-  //   console.log(result);
-  // }
+  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setStatus("typing");
+    setName(event.target.value);
+  }
+
+  function handleRatingChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setStatus("typing");
+    setRating(event.target.valueAsNumber);
+  }
+
+  function handlePhoneNumberChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setStatus("typing");
+    setPhoneNumber(event.target.value);
+  }
+
+  function handleInstagramHandleChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setStatus("typing");
+    setInstagramHandle(event.target.value);
+  }
+
+  function handeleSitInChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setStatus("typing");
+    setSitIn((prevSitIn) => !prevSitIn);
+  }
+
+  function handleCurbsidePickupChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setStatus("typing");
+    setCurbsidePickup((prevCurbsidePickup) => !prevCurbsidePickup);
+  }
+
+  function handleDeliveryChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setStatus("typing");
+    setDelivery((prevDelivery) => !prevDelivery);
+  }
+
+  function handleTakeOutChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setStatus("typing");
+    setTakeOut((prevTakeout) => !prevTakeout);
+  }
 
   return (
     <>
@@ -150,51 +205,40 @@ export default function CreateStore() {
               add your store ☕
             </h1>
           </section>
-          <form onSubmit={handleGetValues} className="space-y-8">
+          <form onSubmit={onSubmit} className="space-y-8">
             <div className="space-y-2">
               <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                 Store Name
               </label>
-              <Controller
-                name="name"
-                control={control}
-                render={({ field }) => {
-                  return (
-                    <input
-                      className="flex h-10 w-full rounded-md border-2 border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none hover:border-transparent focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                      type="text"
-                      placeholder="Blue Bottle Coffee"
-                      {...field}
-                    />
-                  );
-                }}
+              <input
+                value={name}
+                type="text"
+                id="name"
+                onChange={handleNameChange}
+                className="flex h-10 w-full rounded-md border-2 border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none hover:border-transparent focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                placeholder="Blue Bottle Coffee"
               />
-              <div className="text-destructive text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              {/* <div className="text-destructive text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                 {errors.name?.message && <p>{errors.name?.message}</p>}
-              </div>
+              </div> */}
             </div>
             <div className="space-y-2">
               <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                 Rating ⭐
               </label>
-              <Controller
+              <select
                 name="rating"
-                control={control}
-                render={({ field }) => {
-                  return (
-                    <select
-                      className="flex h-10 w-2/12 rounded-md border-2 border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none hover:border-transparent focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                      {...field}
-                    >
-                      <option value="5">5</option>
-                      <option value="4">4</option>
-                      <option value="3">3</option>
-                      <option value="2">2</option>
-                      <option value="1">1</option>
-                    </select>
-                  );
+                onChange={() => {
+                  setRating;
                 }}
-              />
+                className="flex h-10 w-2/12 rounded-md border-2 border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none hover:border-transparent focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              >
+                <option value="5">5</option>
+                <option value="4">4</option>
+                <option value="3">3</option>
+                <option value="2">2</option>
+                <option value="1">1</option>
+              </select>
               <div className="text-destructive text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                 {errors.rating?.message && <p>{errors.rating?.message}</p>}
               </div>
@@ -225,7 +269,14 @@ export default function CreateStore() {
                 )}
               </div>
             </div>
-            <div className="space-y-2">
+            <FormField
+              label="Instagram"
+              type="type"
+              name="instagramHandle"
+              placeholder="@"
+              handleChangeFunc="handlet"
+            />
+            {/* <div className="space-y-2">
               <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                 Instagram
               </label>
@@ -252,7 +303,7 @@ export default function CreateStore() {
                   <p>{errors.instagramHandle?.message}</p>
                 )}
               </div>
-            </div>
+            </div> */}
             <div className="space-y-2">
               <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                 Avatar
